b82a52a365535a37eac9686a20c094a0dac9a305
ad045 <adrian.dendorfer@gmail.com>
2023-11-17 16:22:58 +0100

    First commit: - Updated .gitignore - Changed download_data_lemon.py, to include matching names etc.

--- download_data_lemon.py
+++ download_data_lemon.py
@@ -1,3 +1,5 @@
+# %% 
+
 import os
 import pathlib
 import urllib.request
@@ -6,17 +8,33 @@ import pandas as pd
 
 DEBUG = False
 url_lemon = ('https://ftp.gwdg.de/pub/misc/MPI-Leipzig_Mind-Brain-Body-LEMON'
-             '/EEG_MPILMBB_LEMON/EEG_Raw_BIDS_ID/')
+             '/EEG_MPILMBB_LEMON/EEG_Raw_BIDS_ID')                             # removed a dash at the end
 
 lemon_info = pd.read_csv(
   "./META_File_IDs_Age_Gender_Education_Drug_Smoke_SKID_LEMON.csv")
 
-data_path = pathlib.Path("/storage/store3/data/LEMON_RAW")
+#%% start: added by ad                                                         # ad
+matching_df = pd.read_csv("name_match_LEMON.csv")                              
+
+lemon_info["ID"] = None                                                        # ad
+
+for (initial_ID, INDI_ID) in zip(matching_df.Initial_ID, matching_df.INDI_ID): # ad
+    lemon_info.loc[lemon_info["Unnamed: 0"] == INDI_ID,"ID"] = initial_ID      # ad
+
+# end
+#%%
+
+data_path = pathlib.Path("storage/store3/data/LEMON_RAW")                      # ad: removed /, since that would need higher permission
 
 if not data_path.exists():
     os.makedirs(data_path)
 
 subjects = sorted(lemon_info.ID)
+
+# print(subjects)
+
+# %% 
+
 if DEBUG:
     subjects = subjects[:1]
 
