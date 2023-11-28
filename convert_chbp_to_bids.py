33ee5ab10839946e31a5bc9413cbbfd24dc76295
ad045 <adrian.dendorfer@gmail.com>
2023-11-28 10:14:35 +0100

    CHBP ran successfully 2.

--- convert_chbp_to_bids.py
+++ convert_chbp_to_bids.py
@@ -12,7 +12,7 @@ new 'particpants.tsv' to the root path.
 """
 
 demographics_root = pathlib.Path(
-    '/storage/store3/data/CHBMP_Cognitive_Scales')
+    '../raw_data/storage/store5/CHBMP_Cognitive_Scales')
 
 subjects_df = pd.read_csv(
     demographics_root / 'Demographic_data.csv', skiprows=1)
@@ -21,5 +21,9 @@ subjects_df = subjects_df.iloc[:, :3]
 subjects_df.columns = ['participant_id', 'sex', 'age']
 subjects_df['participant_id'] = 'sub-' + subjects_df.participant_id
 subjects_df.to_csv(
-    cfg.bids_root / 'participants.tsv', sep='\t',
-    index=False)
+    # cfg.bids_root / 'participants.tsv', sep='\t',
+    # index=False)
+    "processed_CHBM/participants.csv", sep='\t', index=False)
+
+
+# raw_data/storage/participants.tsv (participants file for CHBP)
